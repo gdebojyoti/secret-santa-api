c806d8ee98dd5f35378837511ae3ec5fc8ead99a
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2021-12-19 17:15:26 +0530

    Missing file fix.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 31291
 
-const sendEmail = require('./services/sendEmail')
+// const sendEmail = require('./services/sendEmail')
 const routes = require('./routes')
 
 app.use(express.json())
