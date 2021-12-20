d142d5777fa9e635ca8ce17dc2732c63fcbe85cb
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2021-12-21 02:39:03 +0530

    Avoid self gifts

--- services/mapUsers.js
+++ services/mapUsers.js
@@ -1,6 +1,8 @@
 module.exports = users => {
   const ids = []
   const mapping = {}
+
+  // assign random indexes to all users; populate ids & mapping
   const indexedUsers = users.map((user, index) => {
     const id = index + 'x' + Math.round(Math.random() * 100000)
     ids.push(id)
@@ -15,6 +17,28 @@ module.exports = users => {
   for (let i = 0, limit = ids.length; i < limit; i++) {
     const randomIndex = Math.floor(Math.random() * ids.length)
     mapping[backupIds[i]] = ids.splice(randomIndex, 1)[0]
+  }
+
+  // avoid self gifts
+  const selfGiftees = backupIds.filter(id => mapping[id] === id)
+
+  if (selfGiftees.length > 1) {
+    selfGiftees.forEach((id, index) => {
+      mapping[id] = selfGiftees[index === selfGiftees.length - 1 ? 0 : index + 1]
+    })
+  }
+  if (selfGiftees.length === 1) {
+    // exchange with first santa
+    if (selfGiftees[0] !== backupIds[0]) {
+      const temp = mapping[selfGiftees[0]]
+      mapping[selfGiftees[0]] = mapping[backupIds[0]]
+      mapping[backupIds[0]] = temp
+    } else {
+      // exchange with 2nd santa
+      const temp = mapping[selfGiftees[0]]
+      mapping[selfGiftees[0]] = mapping[backupIds[1]]
+      mapping[backupIds[1]] = temp
+    }
   }
 
   console.log("mapping", mapping)
