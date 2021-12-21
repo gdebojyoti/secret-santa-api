d7da55ff3178176d6681f90a422642d1e53bfa64
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2021-12-21 17:53:13 +0530

    cookie sameSite none

--- routes/login.js
+++ routes/login.js
@@ -45,7 +45,7 @@ const service = async (req, res) => {
 
   const { token } = userData
 
-  res.cookie('login_auth_token', token, { overwrite: true, secure: true, sameSite: false })
+  res.cookie('login_auth_token', token, { overwrite: true, secure: true, sameSite: 'None' })
   res.json({
     status: 0,
     message: 'logged in successfully'
